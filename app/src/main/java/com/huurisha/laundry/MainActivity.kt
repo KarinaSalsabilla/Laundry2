98da7c53333f3bc571fa26564be8bbb7799b7336
LENOVO <ayixdjoko@gmail.com>
2025-01-11 09:53:27 +0700

    menambahkan logika dan merapikan potrait

--- app/src/main/java/com/huurisha/laundry/MainActivity.kt
+++ app/src/main/java/com/huurisha/laundry/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.huurisha.laundry
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Menyesuaikan padding untuk sistem bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Referensi TextView
+        val helloTextView: TextView = findViewById(R.id.halo)
+        val dateTextView: TextView = findViewById(R.id.tanggal)
+
+        // Mengatur tanggal hari ini
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val currentDate = dateFormat.format(calendar.time)
+        dateTextView.text = currentDate
+
+        // Mengatur pesan berdasarkan waktu
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val greeting = when {
+            hour in 5..11 -> "Selamat Pagi, Karina"
+            hour in 12..14 -> "Selamat Siang, Karina"
+            hour in 15..17 -> "Selamat Sore, Karina"
+            else -> "Selamat Malam, Karina"
+        }
+        helloTextView.text = greeting
     }
 }
