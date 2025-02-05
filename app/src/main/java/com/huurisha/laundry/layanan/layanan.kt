b83aa08fdc3900fe0667799ebef9648175cd3bca
LENOVO <ayixdjoko@gmail.com>
2025-02-05 08:54:48 +0700

    memodifikasi carddatapelanggan,cardadatapegawai,dll.

--- app/src/main/java/com/huurisha/laundry/layanan/layanan.kt
+++ app/src/main/java/com/huurisha/laundry/layanan/layanan.kt
@@ -11,7 +11,7 @@ class layanan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_layanan)
+        setContentView(R.layout.activity_data_layanan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
